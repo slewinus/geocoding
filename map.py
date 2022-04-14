b25f85b3abe1d8a92d9ff5349523911ecb47095a
oscar <oscar.robert>
2022-04-14 10:55:47 +0200

    log + ajout de la map

---
+++ map.py
@@ -0,0 +1,15 @@
+import gmplot
+import pandas as pd
+
+dataset = pd.read_csv('out.csv')
+latitudes = dataset.loc[:, 'latitude']
+longitudes = dataset.loc[:, 'longitude']
+min_latitude = latitudes.min()
+max_latitude = latitudes.max()
+min_longitude = longitudes.min()
+max_longitude = longitudes.max()
+
+gmap = gmplot.GoogleMapPlotter(35, -102, 5)
+gmap.scatter(latitudes[:1000], longitudes[:1000], 'blue', size = 10)
+gmap.apikey = 'AIzaSyBgYjmLWQd4j0N_WUL68lJcUNGCxN_dyVg'
+gmap.draw('gmplot.html')
